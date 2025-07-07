ece2844cd72d1b91dcd516868c6a0106dc6d7bb7
clouduser2025 <hussainfarheen915@gmail.com>
2025-07-07 12:37:13 +0530

    ASDFG

--- src/components/BalloonIntro.jsx
+++ src/components/BalloonIntro.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../balloonintro.css';
-import logo from '../assets/image19.png';       // Top-left logo and watermark
-import balloon from '../assets/balloon.png';    // Thumbnail balloon
+import logo from '../assets/image19.png';       // Using the same logo path as your code
+import balloon from '../assets/balloon.png';    // Using the same balloon path as your code
 
-export default function BalloonIntro() {
+export default function GameIntro() {
   const navigate = useNavigate();
 
   return (
@@ -27,24 +27,24 @@ export default function BalloonIntro() {
         style={{ backgroundImage: `url(${logo})` }}
       ></div>
 
-      {/* 🎈 Small thumbnail balloon (image 3 in Figma) */}
+      {/* Small thumbnail balloon */}
       <div
         className="balloon-image"
         style={{ backgroundImage: `url(${balloon})` }}
       ></div>
 
-      {/* 📦 Main white box */}
+      {/* Main white box */}
       <div className="intro-box">
-        <div className="intro-title">Balloon Analogue Risk Task</div>
+        <div className="intro-title">My Awesome Game</div>
         <div className="intro-description">
-          In this task, you will inflate balloons to earn points. Each pump increases your potential earnings, but if the balloon explodes, you lose the points. Balloons can explode at any moment. This game helps assess risk-taking behavior. Try a practice round first.
+          In this game, you will face exciting challenges to earn rewards. Each action increases your score, but beware of unexpected twists that could reset your progress. This game tests your strategy and luck. Try a practice round to get started!
         </div>
         <div
           className="start-btn"
-          onClick={() => navigate('/balloon')}
+          onClick={() => navigate('/game')}
           role="button"
           tabIndex={0}
-          onKeyDown={(e) => e.key === 'Enter' && navigate('/balloon')}
+          onKeyDown={(e) => e.key === 'Enter' && navigate('/game')}
         >
           Practice
         </div>

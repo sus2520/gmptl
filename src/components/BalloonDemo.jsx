301665047e629baba59b42baa0c4954593c578d8
clouduser2025 <hussainfarheen915@gmail.com>
2025-07-07 17:17:11 +0530

    ASDFG

--- src/components/BalloonDemo.jsx
+++ src/components/BalloonDemo.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../balloondemo.css';
-import logoImage from './assets/image19.png';
-import balloonImage from './assets/balloon.png';
+import logoImage from '../assets/image19.png';
+import balloonImage from '../assets/balloon.png';
 
 const BalloonDemo = () => {
   const [pumps, setPumps] = useState(0);

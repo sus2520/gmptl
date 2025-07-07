07a9e0a0ce36e968138cd0f7e121f5b603379d82
clouduser2025 <hussainfarheen915@gmail.com>
2025-07-07 12:06:49 +0530

    ASDFG

--- src/components/BalloonIntro.jsx
+++ src/components/BalloonIntro.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../balloonintro.css';
-import logo from '../assets/image19.png';     // Top-left logo and watermark
-import balloon from '../assets/balloon.png'; // Balloon graphic
+import logo from '../assets/image19.png'; // Logo & Watermark
+import balloon from '../assets/balloon.png'; // Balloon background
 
 export default function BalloonIntro() {
   const navigate = useNavigate();
@@ -15,31 +15,36 @@ export default function BalloonIntro() {
         <div className="orange-bar"></div>
       </div>
 
-      {/* Top-left logo */}
+      {/* Full background balloon */}
       <div
-        className="logo-image"
-        style={{ backgroundImage: `url(${logo})` }}
+        className="balloon-background-image"
+        style={{ backgroundImage: `url(${balloon})` }}
       ></div>
 
-      {/* Center watermark */}
+      {/* Watermark center faded */}
       <div
         className="watermark-image"
         style={{ backgroundImage: `url(${logo})` }}
       ></div>
 
-      {/* Balloon overlay on same position */}
+      {/* Logo top-left */}
+      <div
+        className="logo-image"
+        style={{ backgroundImage: `url(${logo})` }}
+      ></div>
+
+      {/* Small balloon image (top center) */}
       <div
         className="balloon-image"
         style={{ backgroundImage: `url(${balloon})` }}
       ></div>
 
-      {/* Main content box */}
+      {/* Main box */}
       <div className="intro-box">
         <div className="intro-title">Balloon Analogue Risk Task</div>
         <div className="intro-description">
           In this task, you will inflate balloons to earn points. Each pump increases your potential earnings, but if the balloon explodes, you lose the points. Balloons can explode at any moment. This game helps assess risk-taking behavior. Try a practice round first.
         </div>
-
         <div
           className="start-btn"
           onClick={() => navigate('/balloon')}
